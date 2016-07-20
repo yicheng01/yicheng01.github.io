e5483b68e205bcb0059f5f309c7a7d78aa7a5849
EasonWang01 <jason40115@hotmail.com>
2016-07-20 15:55:29 +0800

    add

--- script/index.js
+++ script/index.js
@@ -92,7 +92,7 @@ var step = function(i){
 	 if(articles.length==TotalArticleNumber){
 	  	  Array.prototype.forEach.call(document.getElementsByClassName('title'),function(item){
 			item.addEventListener('click',function(){
-				history.pushState({ page: "article/test"+item.id },"", "article/test"+item.id+'.html');
+				history.replaceState({ page: "article/test"+item.id },"", "article/test"+item.id+'.html');
 				if(document.getElementById('content')!==null){
 					console.log(document.getElementById('realArticle').childNodes)
 					document.getElementById('realArticle').removeChild(document.getElementById('content')); 
@@ -169,3 +169,6 @@ sdf().then(function(){
 
 
 */
+window.onbeforeunload = function(e) {
+  console.log('reload')
+};
