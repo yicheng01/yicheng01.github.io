918ce2643083d5be6a3a17bb2d70bc66cfbf2df9
EasonWang01 <jason40115@hotmail.com>
2016-07-20 09:07:52 +0800

    add

--- script/index.js
+++ script/index.js
@@ -116,7 +116,14 @@ for(let i = TotalArticleNumber;i>0;i--){
 	  		
 	  		document.getElementById('realArticle').innerHTML =converter.makeHtml(e);
 	  		*/
-	  		
+	  		    (function() {  // DON'T EDIT BELOW THIS LINE
+        var d = document, s = d.createElement('script');
+        
+        s.src = 'https://yicheng01blog.disqus.com/embed.js';
+        
+        s.setAttribute('data-timestamp', +new Date());
+        (d.head || d.body).appendChild(s);
+    })();
   };
     /**
      *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
@@ -128,14 +135,7 @@ for(let i = TotalArticleNumber;i>0;i--){
         this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
     };
     */
-    (function() {  // DON'T EDIT BELOW THIS LINE
-        var d = document, s = d.createElement('script');
-        
-        s.src = 'https://yicheng01blog.disqus.com/embed.js';
-        
-        s.setAttribute('data-timestamp', +new Date());
-        (d.head || d.body).appendChild(s);
-    })();
+
 
 
 
