6ab401f54cfff636840acccc9fb19e59dc5c59d1
EasonWang01 <jason40115@hotmail.com>
2016-07-20 09:34:18 +0800

    add

--- script/index.js
+++ script/index.js
@@ -119,7 +119,7 @@ for(let i = TotalArticleNumber;i>0;i--){
 	  		    (function() {  // DON'T EDIT BELOW THIS LINE
         var d = document, s = d.createElement('script');
         
-        s.src = 'https://yicheng01blog.disqus.com/embed.js';
+        s.src = '//yicheng01blog.disqus.com/embed.js';
         
         s.setAttribute('data-timestamp', +new Date());
         (d.head || d.body).appendChild(s);
