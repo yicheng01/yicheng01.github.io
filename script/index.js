ee49f3a205789b45d838261b7242412af0f48d67
EasonWang01 <jason40115@hotmail.com>
2016-07-20 12:41:12 +0800

    add

--- script/index.js
+++ script/index.js
@@ -80,11 +80,9 @@ function loadhtml(url,callback) {
 
 };
 
-function readloops(){
-  var i = TotalArticleNumber;
-
-  while (i > 0) {
-    loadhtml('./article/test'+i+'.md',function(e){
+var step = function(i){
+   if( i > 0 ) {
+	loadhtml('./article/test'+i+'.md',function(e){
 	 sdf(i,e)
 	 articles.push(e);
 
@@ -123,24 +121,14 @@ function readloops(){
 		
 
 	  	}
-
+	  		step(i-1);
 	 })
-      i--;
-	  		
-	};	
-	
+
+
+   }
 };
-    /**
-     *  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
-     *  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables
-     */
-    /*
-    var disqus_config = function () {
-        this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
-        this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
-    };
-    */
-readloops();
+
+step(TotalArticleNumber);
 
 
 
